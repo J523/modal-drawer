afb6a5b6bbe7c2fa9ce2679a2306832a1948d03f
J523 <48486122+J523@users.noreply.github.com>
2020-02-10 16:50:51 +0100

    Initial commit

---
+++ src/demo.tsx
@@ -0,0 +1,172 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Drawer from "@material-ui/core/Drawer";
+import Button from "@material-ui/core/Button";
+import List from "@material-ui/core/List";
+import Divider from "@material-ui/core/Divider";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import InboxIcon from "@material-ui/icons/MoveToInbox";
+import MailIcon from "@material-ui/icons/Mail";
+import { fade } from "@material-ui/core/styles/colorManipulator";
+const useStyles = makeStyles({
+  list: {
+    width: 250
+  },
+  fullList: {
+    width: "auto"
+  },
+  root: {
+    position: "relative",
+    padding: "100px"
+  },
+  modal: {
+    padding: "100px"
+  },
+  paperAnchorTop: {
+    padding: "100px",
+    position: "absolute",
+    bottom: 0,
+    left: 100
+  },
+  backDrop: {
+    backgroundColor: fade("#f5ced3", 0.4)
+  }
+});
+
+export default function TemporaryDrawer() {
+  const classes = useStyles();
+  const [state, setState] = React.useState({
+    top: false,
+    left: false,
+    bottom: false,
+    right: false
+  });
+
+  const [anchorEl, setanchorEl] = React.useState<
+    Element | ((element: Element) => Element)
+  >(null);
+
+  type DrawerSide = "top" | "left" | "bottom" | "right";
+  const toggleDrawer = (side: DrawerSide, open: boolean) => (
+    event: React.KeyboardEvent | React.MouseEvent
+  ) => {
+    if (
+      event.type === "keydown" &&
+      ((event as React.KeyboardEvent).key === "Tab" ||
+        (event as React.KeyboardEvent).key === "Shift")
+    ) {
+      return;
+    }
+
+    setState({ ...state, [side]: open });
+    setanchorEl(event.currentTarget);
+  };
+
+  const sideList = (side: DrawerSide) => (
+    <div
+      className={classes.list}
+      role="presentation"
+      onClick={toggleDrawer(side, false)}
+      onKeyDown={toggleDrawer(side, false)}
+    >
+      <List>
+        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>
+              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+            </ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {["All mail", "Trash", "Spam"].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>
+              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+            </ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
+
+  const fullList = (side: DrawerSide) => (
+    <div
+      className={classes.fullList}
+      role="presentation"
+      onMouseLeave={toggleDrawer(side, false)}
+      onKeyDown={toggleDrawer(side, false)}
+    >
+      <List>
+        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>
+              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+            </ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {["All mail", "Trash", "Spam"].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>
+              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+            </ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
+
+  return (
+    <div style={{ position: "relative" }}>
+      <Button onClick={toggleDrawer("left", true)}>Open Left</Button>
+      <Button onClick={toggleDrawer("right", true)}>Open Right</Button>
+      <Button onMouseEnter={toggleDrawer("top", true)}>Open Top</Button>
+      <Button onClick={toggleDrawer("bottom", true)}>Open Bottom</Button>
+      <Drawer open={state.left} onClose={toggleDrawer("left", false)}>
+        {sideList("left")}
+      </Drawer>
+      <Drawer
+        anchor="top"
+        open={state.top}
+        onClose={toggleDrawer("top", false)}
+        classes={{
+          root: classes.root,
+          paperAnchorTop: classes.paperAnchorTop,
+          modal: classes.modal
+        }}
+        variant="temporary"
+        BackdropProps={{
+          classes: {
+            root: classes.backDrop
+          }
+        }}
+      >
+        {fullList("top")}
+      </Drawer>
+      <Drawer
+        anchor="bottom"
+        open={state.bottom}
+        onClose={toggleDrawer("bottom", false)}
+      >
+        {fullList("bottom")}
+      </Drawer>
+      <Drawer
+        anchor="right"
+        open={state.right}
+        onClose={toggleDrawer("right", false)}
+      >
+        {sideList("right")}
+      </Drawer>
+    </div>
+  );
+}
